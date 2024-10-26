dafdc3672b987934a96e0ebc2cb35ca5ccf9e116
mlaferriere2004 <mikael.laferriere2004@gmail.com>
2024-10-26 15:37:53 -0400

    Nouvelle photo + Modale fait

--- src/scripts/components/Modale.js
+++ src/scripts/components/Modale.js
@@ -3,10 +3,42 @@ export default class Modale {
     this.element = element;
 
     this.medias = this.element.querySelectorAll('.has_modale');
-    console.log(this.medias);
+    this.modale = document.querySelector('.modale');
+    this.modaleMediaContainer = this.modale.querySelector('.modale_media');
 
     this.init();
   }
 
-  init() {}
+  init() {
+    this.applyEvents();
+  }
+
+  applyEvents() {
+    //apply event to modale
+    this.modale.addEventListener('click', this.closeModale.bind(this));
+
+    //apply click events to medias
+    for (let i = 0; i < this.medias.length; i++) {
+      const element = this.medias[i];
+      element.addEventListener('click', this.openModale.bind(this));
+    }
+  }
+
+  openModale(event) {
+    const mediaClone = event.target.cloneNode(true);
+
+    mediaClone.style.cursor = 'pointer';
+
+    this.modaleMediaContainer.innerHTML = '';
+
+    this.modaleMediaContainer.appendChild(mediaClone);
+
+    this.modale.classList.toggle('modale_active');
+  }
+
+  closeModale() {
+    this.modale.classList.toggle('modale_active');
+
+    this.modaleMediaContainer.innerHTML = '';
+  }
 }
